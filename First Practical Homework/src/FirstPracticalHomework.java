1698def9664166f35fb64563e325f96720055c23
DELL <DELL@DESKTOP-IF2PEG5>
2021-02-04 14:53:07 +0300

    First Commit

---
+++ First Practical Homework/src/FirstPracticalHomework.java
@@ -0,0 +1,11 @@
+// This program is written by Mohammed Shaman
+// Data : Feb4 , 2021
+// Course Name : 212CSS-4 Object oriented programming 
+public class FirstPracticalHomework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		System.out.println("Mohammed Shaman");
+	}
+
+}
